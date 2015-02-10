4b6c9073cf4a4cae16c41a57e00791fe9b95ad78
Ace Talibong <icee.tal007@gmail.com>
2015-02-10 18:19:28 +0800

    Added Tag, Task, TrashBin, and User classes

---
+++ src/things/User.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package things;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author AceTalibong
+ */
+public class User {
+    String username;
+    private String password;
+    String name;
+    ArrayList projects;
+    ArrayList areaOfResponsibilities;
+    
+    public User(String username, String password, String name, Project project, AreaOfResponsibility aor){
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        projects = new ArrayList <Project>();
+        areaOfResponsibilities = new ArrayList <AreaOfResponsibilty>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void addProject(Project project){
+        projects.add(project);
+    }
+    
+    public void addAreaOfResponsibility(AreaOfResponsibility aor){
+        areaOfResponsibilities.add(aor);
+    }
+}
